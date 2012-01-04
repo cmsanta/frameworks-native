db43ef0d9241a1155ea20cae4d7a7a87b3646afa
Pauli Nieminen <pauli.nieminen@linux.intel.com>
2012-01-04 16:25:10 +0200

    egl: Add NULL check for eglGetconfigs num_config parameter
    EGL 1.4 specification says in 3.4.1:
    "An EGL_BAD_PARAMETER error is generated if num config is NULL."
    
    We have to check for the condition and return the required error to
    application.
    
    Change-Id: Ib42709f0420161ce661536394d4c0779bc62be57
    Original-Change-Id: I4fe670c4362f4ae494f91d69ae105e29ccf3c769
    Signed-off-by: Pauli Nieminen <pauli.nieminen@linux.intel.com>

--- opengl/libagl/egl.cpp
+++ opengl/libagl/egl.cpp
@@ -1459,6 +1459,9 @@ EGLBoolean eglGetConfigs(   EGLDisplay dpy,
     if (egl_display_t::is_valid(dpy) == EGL_FALSE)
         return setError(EGL_BAD_DISPLAY, EGL_FALSE);
 
+    if (ggl_unlikely(num_config==0))
+        return setError(EGL_BAD_PARAMETER, EGL_FALSE);
+
     GLint numConfigs = NELEM(gConfigs);
     if (!configs) {
         *num_config = numConfigs;
